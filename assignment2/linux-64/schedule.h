8ff5a9c22170a7ad9e202df6f4cb33558ec3e584
Maico Timmerman <maico.timmerman@gmail.com>
2014-04-15 12:37:53 +0200

    Fixed style.

--- assignment2/linux-64/schedule.h
+++ assignment2/linux-64/schedule.h
@@ -2,77 +2,77 @@
    Deze header file bevat de voor de opgave over high-level scheduling en
    geheugen-allocatie benodigde definities
 
-   Auteur:	Dick van Albada
-		Vakgroep Computersystemen
-		Kruislaan 403
-   Datum:	7 septemebr 2003
-   Versie:	0.3
+   Auteur:  Dick van Albada
+        Vakgroep Computersystemen
+        Kruislaan 403
+   Datum:   7 septemebr 2003
+   Versie:  0.3
 ****************************************************************************/
 
 /****************************************************************************
    De verschillende soorten events:
    NewProcess_event - er is een nieuw proces in de new_proc rij
-	bijgeplaatst.
+    bijgeplaatst.
    Time_event - het lopende proces heeft zijn time-slice opgebruikt.
-	Wordt niet gegenereerd, tenzij je zelf "set_slice" aanroept.
-	Hoort b.v. in een Round-Robin schedule thuis.
+    Wordt niet gegenereerd, tenzij je zelf "set_slice" aanroept.
+    Hoort b.v. in een Round-Robin schedule thuis.
    Ready_event - een proces is klaar met I/O en is weer achteraan de
-	ready_proc rij geplaatst. Voor sommige CPU-scheduling algoritmen
-	een punt om weer een beslissing te nemen.
+    ready_proc rij geplaatst. Voor sommige CPU-scheduling algoritmen
+    een punt om weer een beslissing te nemen.
    IO_event - het lopende proces gaat I/O doen en is achteraan de
-	io_proc rij geplaatst. Als je de volgorde van de processen in
-	de io_proc queue of in de ready queue wil aanpassen, kan dat nu.
+    io_proc rij geplaatst. Als je de volgorde van de processen in
+    de io_proc queue of in de ready queue wil aanpassen, kan dat nu.
    Finish_event - het lopende proces is beeindigd en in de defunct_proc rij
-	geplaatst. Een goede gelegenheid om nieuwe processen toe te laten.
+    geplaatst. Een goede gelegenheid om nieuwe processen toe te laten.
 ****************************************************************************/
 
 typedef enum EVENT {NewProcess_event, Time_event, Ready_event, IO_event,
-		Finish_event} event_type;
+        Finish_event} event_type;
 
 /****************************************************************************
    de structuur "pcb" bevat alle informatie die voor de scheduler
    beschikbaar is.
    SIM_pcb verwijst naar de voor de simulator benodigde informatie en mag
-	   niet worden gewijzigd.
+       niet worden gewijzigd.
    your_admin wordt door de simulator niet gebruikt (aanvankelijk NULL).
-	   Deze pointer is beschikbaar om desgewenst voor de scheduler een
-	   eigen administratie aan de pcb te kunnen koppelen.
+       Deze pointer is beschikbaar om desgewenst voor de scheduler een
+       eigen administratie aan de pcb te kunnen koppelen.
    prev en next worden gebruikt voor het construeren van dubbel verbonden
-	   lijsten. Ze kunnen zowel door de simulator als door de scheduler
-	   worden gewijzigd.
+       lijsten. Ze kunnen zowel door de simulator als door de scheduler
+       worden gewijzigd.
    MEM_need bevat het aantal voor dit proces benodigde longs geheugen.
-	   MEM_need wordt door de simulator ingevuld en mag niet worden
-	   gewijzigd.
+       MEM_need wordt door de simulator ingevuld en mag niet worden
+       gewijzigd.
    MEM_base staat aanvankelijk op -1 en moet door de (hoog-niveau) scheduler
-	   een maal worden gevuld met de begin-locatie in het te gebruiken
-	   geheugen-array. Door jouw scheduler een maal te veranderen.
+       een maal worden gevuld met de begin-locatie in het te gebruiken
+       geheugen-array. Door jouw scheduler een maal te veranderen.
    proc_num is het volgnummer van het proces en wordt door de simulator
-	   gevuld. Niet veranderen.
+       gevuld. Niet veranderen.
 ****************************************************************************/
 
 typedef struct PCB
 {
-    void   	*SIM_pcb;
-    void   	*your_admin;
-    struct PCB 	*prev,
-		*next;
-    long 	MEM_need,
-		MEM_base,
-		proc_num;
+    void    *SIM_pcb;
+    void    *your_admin;
+    struct PCB  *prev,
+        *next;
+    long    MEM_need,
+        MEM_base,
+        proc_num;
 } pcb;
 
 /****************************************************************************
    De wachtrijen.
    Nieuwe processen worden achteraan in de rij new_proc bijgeplaatst.
-	(programma practicumleiding)
+    (programma practicumleiding)
    Na toewijzing van geheugen dienen ze in de ready_proc rij te worden
    geplaatst.
-	(procedure schedule)
+    (procedure schedule)
    Een proces wordt voor de CPU gescheduled door het voorin de ready_proc
    rij te plaatsen. Zonder enige verdere voorzieningen werken de io_proc
    en ready_proc rijen op FCFS basis hun klanten af.
-	(procedure schedule - een andere CPU scheduling dan FCFS vereist
-	dus aanpassingen in de volgorde van processen in deze queues.
+    (procedure schedule - een andere CPU scheduling dan FCFS vereist
+    dus aanpassingen in de volgorde van processen in deze queues.
    Als een proces I/O wil doen (gesimuleerd), komt het in de io_proc rij.
    Laat deze rij met rust.
    Een beeindigd proces komt in de defunct_proc rij. Ruim deze op.
@@ -103,7 +103,7 @@ extern void set_slice(double slice);
    set_slice zorgt er intern voor dat slice steeds minstens 1.0 is, om
    voortgang te garanderen.
    Bij een Time_event wordt voordat de scheduler wordt aangeroepen steeds
-   een set_slice(9.9e12) gedaan om te voorkomen dat de simulator daarop kan 
+   een set_slice(9.9e12) gedaan om te voorkomen dat de simulator daarop kan
    blijven hangen.
    Gebruik voor deze opgave is niet nodig.
 *****************************************************************************/
@@ -124,7 +124,7 @@ long    rm_process(pcb **proces);
    De functie variabele finale wordt door het hoofdprogramma geinitialiseerd
    met een vrijwel lege functie die alleen de tekst "Einde van het programma"
    afdrukt.
-   Hij wordt aangeroepen vlak voor het eind van het programma, als de 
+   Hij wordt aangeroepen vlak voor het eind van het programma, als de
    door het hoofdprogramma verzamelde statistische gegevens zijn afgedrukt.
    Door finale naar een eigen functie van het juiste type te laten verwijzen,
    kun je ervoor zorgen dat je eigen afsluitende routine wordt aangeroepen
--- assignment2/linux-64/scheduler-skeleton.c
+++ assignment2/linux-64/scheduler-skeleton.c
@@ -4,117 +4,96 @@
 #include "schedule.h"
 #include "mem_alloc.h"
 
-/* This file contains a bare bones skeleton for the scheduler function
-   for the second assignment for the OSN course of the 2005 fall
-   semester.
-   Author:
+ /* This file contains a bare bones skeleton for the scheduler function
+  * for the second assignment for the OSN course of the 2005 fall
+  * semester.
+  *
+  * Author
+  * G.D. van Albada
+  * Section Computational Science
+  * Universiteit van Amsterd
+  *
+  * Date:
+  * October 23, 2003
+  * Modified:
+  * September 29, 2005
+  */
 
-   G.D. van Albada
-   Section Computational Science
-   Universiteit van Amsterdam
-
-   Date:
-   October 23, 2003
-   Modified:
-   September 29, 2005
-*/
 
 /* This variable will simulate the allocatable memory */
 
 static long memory[MEM_SIZE];
 
-/* The actual CPU scheduler is implemented here */
+/* TODO:The actual CPU scheduler is implemented here */
 
-static void CPU_scheduler()
-{
+static void CPU_scheduler() {
         /* Insert the code for a MLFbQ scheduler here */
 }
 
 /* The high-level memory allocation scheduler is implemented here */
 
-static void GiveMemory()
-{
+
+static void GiveMemory() {
    int index;
    pcb *proc1, *proc2, *proc3;
 
    proc2 = new_proc;
-   while (proc2)
-   {
+   while (proc2) {
        /* Search for a new process that should be given memory.
-          Insert search code and criteria here.
-          Attempt to allocate as follows:
+        * Insert search code and criteria here.
+        * Attempt to allocate as follows:
         */
        index = mem_get(proc2->MEM_need);
-       if (index >= 0)
-       {
-           /* Allocation succeeded, now put in administration
-            */
+       if (index >= 0) {
+           /* Allocation succeeded, now put in administration */
            proc2->MEM_base = index;
 
-           /* You might want to move this process to the ready
-             queue now
-            */
+           /* You might want to move this process to the ready queue now */
        }
    }
 }
 
-/* Here we reclaim the memory of a process after it
-  has finished */
-
-static void ReclaimMemory()
-{
+/* Here we reclaim the memory of a process after it has finished */
+static void ReclaimMemory() {
     pcb *proc;
 
     proc = defunct_proc;
-    while (proc)
-    {
-        /* Free your own administrative structure if it exists
-         */
-        if (proc->your_admin)
-        {
+    while (proc) {
+        /* Free your own administrative structure if it exists */
+        if (proc->your_admin) {
             free(proc->your_admin);
         }
-        /* Free the simulated allocated memory
-         */
+
+        /* Free the simulated allocated memory */
         mem_free(proc->MEM_base);
         proc->MEM_base = -1;
 
-        /* Call the function that cleans up the simulated process
-         */
+        /* Call the function that cleans up the simulated process */
         rm_process(&proc);
 
-        /* See if there are more processes to be removed
-         */
+        /* See if there are more processes to be removed */
         proc = defunct_proc;
     }
 }
 
 /* You may want to have the last word... */
-
-static void my_finale()
-{
-    /* Your very own code goes here */
+static void my_finale() {
+    /* TODO:Your very own code goes here */
 }
 
 /* The main scheduling routine */
-
-void schedule(event_type event)
-{
+void schedule(event_type event) {
     static int first = 1;
 
-    if (first)
-    {
+    if (first) {
         mem_init(memory);
         finale = my_finale;
         first = 0;
-        /* Add your own initialisation code here
-         */
+        /* Add your own initialisation code here */
     }
 
-    switch (event)
-    {
-    /* You may want to do this differently
-     */
+    switch (event) {
+    /* You may want to do this differently */
     case NewProcess_event:
         GiveMemory();
         break;
